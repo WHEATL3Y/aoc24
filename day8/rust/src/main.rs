962122a113ece74483a6bfe0a98e1a03f5f3cda0
Jacob Christensen <jchristensen888@gmail.com>
2024-12-16 23:27:57 -0700

    day8 rust

--- day8/rust/src/main.rs
+++ day8/rust/src/main.rs
@@ -5,6 +5,14 @@ fn main() {
     let mut file = File::open("../../inputs/day8.test").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let map: Vec<&str> = contents.split("\n").collect();
-
+    let map: Vec<Vec<&str>> = contents.trim().split("\n")
+        .map(|x| x.split("").collect())
+        .collect();
+    
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            print!("{}", map[y][x]);
+        }
+        println!();
+    }
 }
